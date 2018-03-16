510f16eb6acf10420958cae5f5f83d9a0172351a
Justine <869211@students.wits.ac.za>
2018-03-17 00:05:54 +0200

    Added Connect and FTP_Login functions

--- Login_Out/TCPClient3.py
+++ Login_Out/TCPClient3.py
@@ -4,7 +4,28 @@ import sys
 from FTP_Client0 import *
 PI_Port     = 12346  
 
+def Connect(FTP_Client_Interface):
+    FTP_Client_Interface.PI_Socket.connect(self.ServerAddressPI)
+    
+def FTP_Login(FTP_Client_Interface):
+    command = "USER"
+    FTP_Client_Interface.USER(command)
+    command = "PASS"
+    FTP_Client_Interface.PASS(command)
+    if FTP_Client_Interface.loginFlag:
+        return True
+    else:
+        return False
+    
+        
 Client = FTP_Client_Interface()
+while True:
+    login = FTP_Login(Client)
+    if not login:
+        Connect(Client)
+    else:
+        break
+
 Client.CommandProcessor()
 
  
